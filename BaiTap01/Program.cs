5b63d74b90ce55f3d951d4e3ba3a5cb945320437
ntthanh010176 <ntthanh010176@gmail.com>
2023-12-27 11:25:57 +0700

    Add project files.

---
+++ BaiTap01/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTap01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r1; //khai bao r1
+            Rectangle r2; //khai bao r1
+
+            r1 = new Rectangle(); //khoi tao r1
+
+            Console.Write("Cho biet chieu dai:");
+            double length = double.Parse(Console.ReadLine());
+            Console.Write("Cho biet chieu rong:");
+            double width = double.Parse(Console.ReadLine());
+
+            r2 = new Rectangle(length,width); //khoi tao r2
+
+            Console.WriteLine("-------Thong tin hinh chu nhat r1-----------");
+            Console.WriteLine("Chieu dai : {0}\nChieu rong: {1}\nDien tich: {2}\nChu vi: {3}", 
+                r1.getLength(), r1.getWidth(), r1.findArea(), r1.findPerimeter());
+
+            Console.WriteLine("-------Thong tin hinh chu nhat r2-----------");
+            Console.WriteLine("Chieu dai : {0}\nChieu rong: {1}\nDien tich: {2}\nChu vi: {3}",
+                r2.getLength(), r2.getWidth(), r2.findArea(), r2.findPerimeter());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MinhHoa01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhHoa01
+{
+    class HinhTron {
+        //khai bao thuoc tinh 
+        private double bankinh;       
+        //dinh nghia phuong thuc khoi tao
+        public HinhTron()
+        {
+            bankinh = 1.0;
+        }
+        public HinhTron(double bk)
+        {
+            bankinh = bk;
+        }
+
+        //dinh nghia phuong thuc cap nhat (setter)
+        public void setBanKinh(double bk)
+        {
+            bankinh = Math.Abs(bk); 
+        }
+
+        //dinh nghia phuong thuc truy van (getter)
+        public double getBanKinh()
+        {
+            return bankinh;
+        }
+        // dinh nghia cac phuong thuc tinh toan
+        public double tinhDienTich()
+        {
+            return bankinh * bankinh * Math.PI; 
+        }
+        public double tinhChuVi()
+        {
+            return bankinh * 2 * Math.PI;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HinhTron c1 = new HinhTron(); //tao doi tuong hinh tron c1
+            c1.setBanKinh(-3);
+            Console.WriteLine("Hinh tron c1. Ban kinh={0}, Dien tich ={1}, Chu vi={2}",
+               c1.getBanKinh(),  c1.tinhDienTich(), c1.tinhChuVi());           
+
+
+            Console.Write("Cho biet ban kinh:");
+            double bk = double.Parse(Console.ReadLine());
+            HinhTron c2 = new HinhTron(bk);  
+            
+            Console.WriteLine("Hinh tron c2. Ban kinh={0}, Dien tich ={1}, Chu vi={2}", 
+                c2.getBanKinh(), c2.tinhDienTich(), c2.tinhChuVi());
+
+            Console.ReadLine();
+
+        }
+    }
+}

74bb665957e660273451694806b6978a0e89ad3a
ntthanh010176 <ntthanh010176@gmail.com>
2023-12-29 08:20:49 +0700

    bo sung thay doi bai tap 02

--- BaiTap02/Program.cs
+++ BaiTap02/Program.cs
@@ -11,7 +11,18 @@ namespace BaiTap02
         static void Main(string[] args)
         {
             Account my = new Account("900111", "Le Van Teo", 600);
-            Account myFriend = new Account("900112", "Tran Van Ti", 30);
+
+
+            Console.Write("Inter id value:");
+            string id = Console.ReadLine();
+
+            Console.Write("Inter name value:");
+            string name = Console.ReadLine();
+
+            Console.Write("Inter balance value:");
+            int  balance = int.Parse(Console.ReadLine());
+
+            Account myFriend = new Account(id,name,balance);
 
             Console.WriteLine("-----Thong tin 2 tai khoan ban dau------");
             Console.WriteLine("My Account:[ id:{0}, name: {1}, balance: {2}]",
@@ -19,10 +30,18 @@ namespace BaiTap02
             Console.WriteLine("My friend Account:[ id:{0}, name: {1}, balance: {2}]",
                  myFriend.getId(), myFriend.getName(), myFriend.getBalance());
 
+            int amount = 0;
             //rut tien
+            Console.WriteLine("Debit 200 from My Account");
             my.debit(200);
-            my.tranferTo(myFriend, 410);
+            //chuyen tien
+            Console.Write("Inter a amount value to tranfer:");
+            amount = int.Parse(Console.ReadLine());
+            //chuyen khoan tu tai khoan My den MyFriend
+            my.tranferTo(myFriend, amount);
 
+            //chuyen khoan tu tai khoan MyFriend den My
+            myFriend.tranferTo(my, 50);
 
             //ket qua sau khi thuc hien mot so hanh vi (nap, rut, chuyen khoan)
             Console.WriteLine("-----Thong tin 2 tai khoan sau khi thuc hien giao dich-----");
